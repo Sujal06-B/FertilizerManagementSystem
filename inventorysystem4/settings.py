8e2503885e31c2798676064b05f4102f19ddcf66
Sujal06-B <bolkesujal@gmail.com>
2026-01-16 17:08:41 +0530

    Configure for deployment: Add gunicorn/whitenoise

--- inventorysystem4/settings.py
+++ inventorysystem4/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hxdyx8dr6s-5ngjdc0iujhbb#iz7tazs6-=s9=v*%%&z!!6f!2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -110,6 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -121,7 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = (BASE_DIR/"asert/")
+STATIC_ROOT = (BASE_DIR/"staticfiles")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -138,7 +140,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000', 
     'http://localhost:8000',
     'http://127.0.0.1', 
-    'http://localhost'
+    'http://localhost',
+    'https://*.onrender.com'
 ]
 
 CSRF_COOKIE_SECURE = False
